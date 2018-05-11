45c0088b68ad75841a8c96c01cf84e8cb11b3968
kwa52 <kwa52@sfu.ca>
2018-05-11 16:31:33 -0700

    add DineMe page

---
+++ scripts/dine-me.js
@@ -0,0 +1,69 @@
+
+bioMp(document.getElementById('signInPreview'), {
+    url: '../images/dine-me-screenshots/simple-version/1_sign_in375x669.png',
+    view: 'right',
+    scale: '0.6',
+    image: '../scripts/website-mobile-preview-master/images/iphone6_side_right_gold.png'
+});
+
+bioMp(document.getElementById('homePreview'), {
+    url: '../images/dine-me-screenshots/simple-version/1_home375x669.png',
+    view: 'right',
+    scale: '0.6',
+    image: '../scripts/website-mobile-preview-master/images/iphone6_side_right_gold.png'
+});
+
+bioMp(document.getElementById('prmoptPreview'), {
+    url: '../images/dine-me-screenshots/simple-version/3_add_prompt375x669.png',
+    view: 'right',
+    scale: '0.6',
+    image: '../scripts/website-mobile-preview-master/images/iphone6_side_right_white.png'
+});
+
+bioMp(document.getElementById('placePickerPreview'), {
+    url: '../images/dine-me-screenshots/simple-version/place_picker_results375x669.png',
+    view: 'right',
+    scale: '0.6',
+    image: '../scripts/website-mobile-preview-master/images/iphone6_side_right_black.png'
+});
+
+bioMp(document.getElementById('nearbyPreview'), {
+    url: '../images/dine-me-screenshots/simple-version/2_nearby_results375x669.png',
+    view: 'right',
+    scale: '0.6',
+    image: '../scripts/website-mobile-preview-master/images/iphone6_side_right_black.png'
+});
+
+// Add Restaurants Screenshots
+
+bioMp(document.getElementById('addRestaurantStepOne'), {
+    url: '../images/dine-me-screenshots/add-restaurant/2_open_place_picker375x669.png',
+    scale: '0.7',
+    image: '../scripts/website-mobile-preview-master/images/iphone6_front_gold.png'
+});
+
+bioMp(document.getElementById('addRestaurantStepTwo'), {
+    url: '../images/dine-me-screenshots/add-restaurant/3_search_restaurant375x669.png',
+    scale: '0.7',
+    image: '../scripts/website-mobile-preview-master/images/iphone6_front_gold.png'
+});
+
+bioMp(document.getElementById('addRestaurantStepThree'), {
+    url: '../images/dine-me-screenshots/add-restaurant/5_click_drop_down_menu_and_select_category375x669.png',
+    scale: '0.7',
+    image: '../scripts/website-mobile-preview-master/images/iphone6_front_gold.png'
+});
+
+// Look Up Nearby Restaurants Screenshots
+
+bioMp(document.getElementById('nearbyStepOne'), {
+    url: '../images/dine-me-screenshots/simple-version/1_home375x669.png',
+    scale: '0.7',
+    image: '../scripts/website-mobile-preview-master/images/iphone6_front_gold.png'
+});
+
+bioMp(document.getElementById('nearbyStepTwo'), {
+    url: '../images/dine-me-screenshots/simple-version/2_nearby_results375x669.png',
+    scale: '0.7',
+    image: '../scripts/website-mobile-preview-master/images/iphone6_front_gold.png'
+});
